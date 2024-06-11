9e73fe22b088ad9ac7bd293805e336ee9df1d94e
Chandrika Sista <cguntur@gmail.com>
2024-06-11 13:50:20 -0700

    Updated the sequelize variables for Heroku

--- server.js
+++ server.js
@@ -11,19 +11,17 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+const SECRET = process.env.SECRET || 'secret';
 
 // Set up Handlebars.js engine with custom helpers
 const hbs = exphbs.create({ helpers });
 
 const sessionOpitons = {
-    secret: 'gcsdesign_secret_key',
+    secret: SECRET,
     cookie: {
-      maxAge: 100000000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
+      maxAge: 3600000,
     },
-    resave: false,
+    resave: true,
     saveUninitialized: true,
     store: new SequelizeStore({
       db: sequelize
